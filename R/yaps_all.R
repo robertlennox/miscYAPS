57ec0d921386b2867ef518f7cfd930b677268189
robertlennox <robertlennox9@gmail.com>
2023-10-27 21:42:51 -0300

    fixed swim

--- R/yaps_all.R
+++ R/yaps_all.R
@@ -2,6 +2,7 @@
 #' functions to get receiver and tagging metadata into session
 #'
 #' @name swim_yaps
+#' @name yaps_all
 #' @import data.table
 #' @import magrittr
 #' @import dplyr
@@ -19,94 +20,74 @@
 if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
 
 
-swim_yaps<-function(fish_detections, rbi_min, rbi_max, runs=5, silent=T){
-  yaps_all<-function (fish_detections, hydros, sync_model, runs, rbi_min, rbi_max,
-                      transmitter_ID, date)
-  {
-    tryCatch({
-      dat <- yaps::applySync(toa = fish_detections %>% dplyr::filter(lubridate::date(.data$ts) ==
-                                                                       date) %>% droplevels() %>% data.table::setDT() %>%
-                               split(.$tag) %>% purrr::pluck(transmitter_ID), hydros = hydros,
-                             sync_model = sync_model)
-      hydros_yaps <- data.table::data.table(sync_model$pl$TRUE_H)
-      colnames(hydros_yaps) <- c("hx", "hy", "hz")
-      toa <- yaps::getToaYaps(synced_dat = dat, hydros = hydros_yaps,
-                              pingType = "rbi", rbi_min = rbi_min, rbi_max = rbi_max)
-      magicYAPS <- function(x) {
-        tryCatch({
-          yaps::runYaps(yaps::getInp(hydros_yaps, toa,
-                                     E_dist = "Mixture", n_ss = 2, pingType = "rbi",
-                                     sdInits = 1, rbi_min = rbi_min, rbi_max = rbi_max,
-                                     ss_data_what = "est", bbox = NULL), silent = silent,
-                        tmb_smartsearch = TRUE, maxIter = 5000)
-        }, error = function(e) {
-          NA
-        })
-      }
-      YAPS_list <- runs %>% purrr::rerun(magicYAPS())
-      magic <- YAPS_list %>% purrr::map(purrr::pluck(4)) %>%
-        purrr::map(purrr::pluck(1)) %>% bind_cols() %>% t() %>%
-        tidyr::as_tibble %>% dplyr::filter(.data$V1 == min(.data$V1)) %>%
-        as.numeric
-      final_track <- YAPS_list %>% purrr::discard(., ~any(is.na(.x))) %>%
-        purrr::compact() %>% purrr::keep(., purrr::as_mapper(~.x$obj %>%
-                                                               purrr::pluck(1) == magic)) %>% purrr::pluck(1) %>%
-        purrr::pluck(8) %>% tidyr::as_tibble %>% dplyr::mutate(dat %>%
-                                                                 dplyr::distinct(.data$tag))
-    }, error = function(e) {
-      NA
-    })
-  }
+yaps_all<-function(transmitter_ID, date, runs, rbi_min, rbi_max){
+  tryCatch({
+    dat <- applySync(toa=fish_detections %>% # sync model for one download
+                       dplyr::filter(date(ts)==date) %>%
+                       droplevels() %>%
+                       setDT %>%
+                       split(.$tag) %>%
+                       pluck(transmitter_ID),
+                     hydros=hydros,
+                     sync_model=sync_model)
+    hydros_yaps <- data.table::data.table(sync_model$pl$TRUE_H)
+    colnames(hydros_yaps) <- c('hx','hy','hz')
+    rbi_min <- rbi_min
+    rbi_max <- rbi_max
+    toa <- getToaYaps(synced_dat=dat,
+                      hydros=hydros_yaps,
+                      pingType='rbi',
+                      rbi_min=rbi_min,
+                      rbi_max=rbi_max)
+    nobs <- apply(toa, 1, function(k) sum(!is.na(k)))
+    ####
+    # YAPS
+    ##
+    magicYAPS<-function(x){
+      tryCatch({runYaps(
+        getInp(hydros_yaps,
+               toa,
+               E_dist="Mixture",
+               n_ss=2,
+               pingType="rbi",
+               sdInits=1,
+               rbi_min=rbi_min,
+               rbi_max=rbi_max,
+               ss_data_what="est",
+               bbox=NULL),
+        silent=F,
+        tmb_smartsearch=TRUE,
+        maxIter=5000)},
+        error=function(e){NA})}
+    YAPS_list<-runs %>%
+      rerun(magicYAPS())
+    magic<-YAPS_list %>%  # the magic number
+      purrr::map(purrr::pluck(4)) %>% # get the AIC cols
+      purrr::map(purrr::pluck(1)) %>% # take the number
+      bind_cols() %>% # make a df
+      t() %>% # oops wrong order
+      as_tibble %>% # obv
+      dplyr::filter(V1==min(V1)) %>% # get the min val
+      as.numeric# make it a number
+    final_track<-YAPS_list %>%
+      purrr::discard(., ~any(is.na(.x))) %>%
+      purrr::compact() %>%
+      keep(., as_mapper(~.x$obj %>% # keep only the run with the lowest AIC
+                          pluck(1) == magic)) %>%
+      pluck(1) %>%
+      pluck(8) %>%
+      as_tibble %>%
+      mutate(dat %>% distinct(tag))},
+    error=function(e){NA})
+}
 
-  function (fish_detections, hydros, sync_model, rbi_min, rbi_max,
-            transmitter_ID, date, runs)
-  {
-    tryCatch({
-      dat <- yaps::applySync(toa = fish_detections %>% dplyr::filter(lubridate::date(.data$ts) ==
-                                                                       date) %>% droplevels() %>% data.table::setDT() %>%
-                               split(.$tag) %>% purrr::pluck(transmitter_ID), hydros = hydros,
-                             sync_model = sync_model)
-      hydros_yaps <- data.table::data.table(sync_model$pl$TRUE_H)
-      colnames(hydros_yaps) <- c("hx", "hy", "hz")
-      toa <- yaps::getToaYaps(synced_dat = dat, hydros = hydros_yaps,
-                              pingType = "rbi", rbi_min = rbi_min, rbi_max = rbi_max)
-      magicYAPS <- function(x) {
-        tryCatch({
-          yaps::runYaps(yaps::getInp(hydros_yaps, toa,
-                                     E_dist = "Mixture", n_ss = 2, pingType = "rbi",
-                                     sdInits = 1, rbi_min = rbi_min, rbi_max = rbi_max,
-                                     ss_data_what = "est", bbox = NULL), silent = F,
-                        tmb_smartsearch = TRUE, maxIter = 5000)
-        }, error = function(e) {
-          NA
-        })
-      }
-      YAPS_list <- runs %>% purrr::rerun(magicYAPS())
-      magic <- YAPS_list %>% purrr::map(purrr::pluck(4)) %>%
-        purrr::map(purrr::pluck(1)) %>% bind_cols() %>% t() %>%
-        tidyr::as_tibble %>% dplyr::filter(.data$V1 == min(.data$V1)) %>%
-        as.numeric
-      final_track <- YAPS_list %>% purrr::discard(., ~any(is.na(.x))) %>%
-        purrr::compact() %>% purrr::keep(., purrr::as_mapper(~.x$obj %>%
-                                                               purrr::pluck(1) == magic)) %>% purrr::pluck(1) %>%
-        purrr::pluck(8) %>% tidyr::as_tibble %>% dplyr::mutate(dat %>%
-                                                                 dplyr::distinct(.data$tag))
-    }, error = function(e) {
-      NA
-    })
-  }
-
-  tr<-fish_detections %>%
-    dplyr::count(dt=lubridate::date(ts), tag) %>%
+swim_yaps<-function(fish_detections=fish_detections, runs, rbi_min, rbi_max){
+  fish_detections  %>%
+    dplyr::count(dt=date(ts), tag) %>%
     dplyr::filter(n>50) %>%
     dplyr::mutate(i=c(1:nrow(.))) %>%
     dplyr::mutate(tag=factor(tag)) %>%
     split(.$i) %>%
-    purrr::map(., ~yaps_all(transmitter_ID=.$tag, date=.$dt,
-                                      runs=runs, rbi_min=rbi_min, rbi_max=rbi_max,
-                                      fish_detections=fish_detections,
-                                      hydros=hydros,
-                                      sync_model=sync_model)) %>%
+    purrr::map(., ~yaps_all(transmitter_ID=.$tag, date=.$dt, runs=runs, rbi_min, rbi_max)) %>%
     purrr::discard(is.na(.)) %>%
-    bind_rows()
-}
+    dplyr::bind_rows()}
