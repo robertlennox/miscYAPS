551885a409ecabd74615f3c7c2af78259eee219e
robertlennox <robertlennox9@gmail.com>
2023-10-29 10:49:04 -0300

    added sync coverage

--- R/sync.R
+++ R/sync.R
@@ -13,19 +13,58 @@
 #' @param ss_data a data.table of the speed of sound data formatted using yaps::tempToSs
 #' @param HOW_THIN is the number for the eps_threshold.. smaller makes a thinner distribution default is 50
 #' @param keep_rate what proportion of sync tag detections do you want to retain, default is 1 (100%)
+#' @param timekeeper is the idx of the most perfectly fixed hydrophone; refer to hydros data.table for idx numbers
 #' @export
 
 if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
 
 fixed=NULL
 
+synccoverage<-function (inp_sync, plot = FALSE)
+{
+  toa <- inp_sync$dat_tmb_sync$toa
+  nh <- ncol(toa)
+  offset_idx <- inp_sync$dat_tmb_sync$offset_idx
+  toa_long <- data.table::data.table(reshape2::melt(toa))
+  colnames(toa_long) <- c("ping", "h", "toa")
+  toa_long[, `:=`(offset_idx, rep(offset_idx, nh))]
+  sync_coverage <- data.table::data.table(reshape2::melt(with(toa_long[!is.na(toa)],
+                                                              table(h, offset_idx))))
+  colnames(sync_coverage) <- c("h", "offset_idx", "N")
+  if (plot) {
+    p<-sync_coverage %>%
+      dplyr::rename(idx=h) %>%
+      left_join(hydros) %>%
+      group_by(serial, x, y, idx) %>%
+      dplyr::summarise(m=mean(N)) %>%
+      mutate(col=case_when(m<50 & m>10 ~ 50,
+                           m<=10 & m>5 ~ 10,
+                           m<=5 ~ 5,
+                           m>=50 ~ 100)) %>%
+      ggplot(aes(x, y, size=m, colour=col, label=paste0(serial, " idx #", idx)))+
+      geom_point()+
+      theme_classic()+
+      geom_text(colour="red", size=4)+
+      labs(x="UTM X", y="UTM Y", size="mean offset")+
+      theme(legend.position="top", text=element_text(size=18))+
+      scale_colour_gradientn(breaks = c(5, 10, 50, 100),
+                             colors = c("red", "orange", "yellow", "green"))+
+      guides(colour=F)+
+      scale_size_continuous(range=c(5, 7))
+    print(p)
+  }
+  return(sync_coverage)
+}
+
+
+
 sync<-function(hydros, detections, ss_data, HOW_THIN=50, keep_rate=1, ss_data_what="data",
-               exclude_self_detections=T, fixed=fixed){
+               exclude_self_detections=T, fixed=fixed, timekeeper=1){
 
   require(data.table)
   max_epo_diff=250
   min_hydros=2
-  time_keeper_idx=1
+  time_keeper_idx=timekeeper
   fixed_hydros_idx=fixed
   n_offset_day=2
   n_ss_day=2
@@ -51,6 +90,8 @@ sync<-function(hydros, detections, ss_data, HOW_THIN=50, keep_rate=1, ss_data_wh
                          excl_self_detect=excl_self_detect,
                          silent_check=F,
                          ss_data_what=ss_data_what)
+
+  synccoverage(inp_sync, hydros)
 
   sync_model_0 <- sync_model_1 <- sync_model_2 <- sync_model_3 <- sync_model_4 <- NULL
   sync_model_0 <- getSyncModel(inp_sync, silent=TRUE, max_iter=1000, tmb_smartsearch = TRUE)
