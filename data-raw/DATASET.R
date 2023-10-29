11c9cdda99bd13fa0796c149a82d2f5d1865015d
robertlennox <robertlennox9@gmail.com>
2023-10-29 12:26:19 -0300

    pingu

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -9,3 +9,7 @@ usethis::use_data(aur, overwrite = TRUE)
 boats<-readRDS("ws.RDS")
 usethis::use_data(boats, overwrite = TRUE)
 
+
+img <- png::readPNG("yaps pingu.png")
+ping<-grid::grid.raster(img)
+usethis::use_data(ping, overwrite=T)
